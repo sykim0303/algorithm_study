a8ff8be1ff98a5a7b520d783db749002065616bc
sykim <bluespade303@gmail.com>
2023-02-01 21:17:56 +0900

    fix: 10870 문제 번호 수정

--- javascript/boj_10870.js
+++ javascript/boj_10870.js
@@ -1,15 +1,14 @@
 /*
  * BAEKJOON ONLINE JUDGE
  * https://www.acmicpc.net
- * Problem Number: 4673
+ * Problem Number: 10870
  * Level: Bronze II
  * Algorithm: Math, Dynamic Programing
  */
 
 /* Pseudocode *
 입력값이 2 미만일 경우 input을 그대로 출력한다.
-입력값이 2 이상일 경우 입력값 만큼의 크기를 가진 배열을 생성한다.
-배열의 0번쨰 숫자는 0, 1번쨰 숫자는 1로 설정한다.
+0번쨰 숫자가 0, 1번쨰 숫자가 1인 배열을 생성한다.
 배열의 i번째 숫자는 i-1번쨰 숫자와 i-2번쨰 숫자의 합이므로, input만큼 반복하며 배열을 채운다.
 배열의 input번째 숫자를 출력한다.
 */
@@ -22,9 +21,7 @@ const input = Number(
 
 if (input < 2) console.log(input);
 else {
-  const answerArr = new Array(input);
-  answerArr[0] = 0;
-  answerArr[1] = 1;
+  const answerArr = [0, 1];
   for (let i = 2; i < input + 1; i++) {
     answerArr[i] = answerArr[i - 1] + answerArr[i - 2];
   }
